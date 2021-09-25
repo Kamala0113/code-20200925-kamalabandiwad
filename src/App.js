584000aa00d57b2132281c6352aa06f4a83b68ed
kamala0113 <kamala0113@gmail.com>
2021-09-25 17:27:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Car Catalogue
+      </header>
+    </div>
+  );
+}
+
+export default App;
